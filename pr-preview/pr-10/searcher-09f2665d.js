e68fb191e5f01fffd6db6f31c43050cefebe83a4
lkdvos <lkdvos@users.noreply.github.com>
2026-09-22 18:39:43 +0000

    Deploy preview for PR 10 🛫

--- pr-preview/pr-10/searcher-09f2665d.js
+++ pr-preview/pr-10/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d6978923.js',
+                path_to_root + 'searchindex-0e53ae77.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
